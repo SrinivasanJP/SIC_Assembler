b902f1fcca39f7b19653659bc46c178e7775d060
Srinivasan <jpsrinivasan38@gmail.com>
2024-08-13 21:17:54 +0530

    feat: page linked

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Home = ({setPage}) => {
   return (
-    <div>
-        <button className=''>
+    <div className='flex justify-center items-center flex-wrap gap-10 mt-10'>
+        <button onClick={()=>setPage("Pass1Editor")} className='bg-gray-900 p-5 w-[47.5%] rounded-2xl hover:bg-gray-800'>
             Pass 1: Code to Intermediate File & SYMTAB
         </button>
+      
     </div>
   )
 }
