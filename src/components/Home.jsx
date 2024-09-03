1c9f4a90e9ed77646308f655ec07314baecbab05
Srinivasan <jpsrinivasan38@gmail.com>
2024-09-03 10:48:33 +0530

    design chagne

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { common } from '../styles/common'
+import pass1Img from"../assets/pass1.webp"
 const Home = ({setPage}) => {
   return (
     <div className='flex justify-center items-center flex-wrap gap-10 mt-10'>
-        <button onClick={()=>setPage("Pass1Editor")} className='bg-gray-900 p-5 w-[47.5%] rounded-2xl hover:bg-gray-800'>
-            Pass 1: Code to Intermediate File & SYMTAB
-        </button>
+      <div className=' w-[50%] backdrop-blur-md rounded-md p-10 shadow-sm shadow-white'>
+        <img src={pass1Img} alt="pass1 assembler" className={"w-full rounded-md translate-y-10 "} />
+        <button onClick={()=>setPage("Pass1Editor")} className={' text-xl p-5 w-fit '+ common.Buttonblue}>
+              Pass 1: Code to Intermediate File & SYMTAB
+          </button>
+      </div>
+        
       
     </div>
   )
