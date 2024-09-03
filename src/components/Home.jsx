cf445dc69b8704d52c5512913fd8e68764e31465
Srinivasan <jpsrinivasan38@gmail.com>
2024-09-03 23:50:03 +0530

    fix: home style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import pass1Img from"../assets/pass1.webp"
 const Home = ({setPage}) => {
   return (
     <div className='flex justify-center items-center flex-wrap gap-10 mt-10'>
-      <div className=' w-[50%] backdrop-blur-md rounded-md p-10 shadow-sm shadow-white'>
-        <div className=' overflow-hidden'>
-        <img src={pass1Img} alt="pass1 assembler" className={"w-full rounded-md translate-y-10  "} />
+      <div className=' w-[50%] backdrop-blur-md rounded-md px-10 pb-5 pt-3 shadow-sm shadow-white group'>
+        <div className=' overflow-hidden mb-5'>
+        <img src={pass1Img} alt="pass1 assembler" className={"w-full rounded-md translate-y-10 transition-all duration-700 -rotate-6 scale-90 group-hover:rotate-0 hover:scale-100  group-hover:translate-y-0 "} />
         </div>
         
         <button onClick={()=>setPage("Pass1Editor")} className={' text-xl p-5 w-fit '+ common.Buttonblue}>
