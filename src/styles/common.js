c99f8f0e273d83701776004825db326baa5f5fd2
Srinivasan <jpsrinivasan38@gmail.com>
2024-09-03 10:49:08 +0530

    common design

---
+++ src/styles/common.js
@@ -0,0 +1,4 @@
+export const common = {
+    Buttonblue:"text-white text-left px-5 py-2 rounded-md bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)]",
+    textNabula: 'bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] bg-clip-text text-transparent font-extrabold text-3xl'
+}
