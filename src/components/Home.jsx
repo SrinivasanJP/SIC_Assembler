1597b2915d3c792f38478db11e2a346ece88a5d9
Srinivasan <jpsrinivasan38@gmail.com>
2024-09-03 23:19:31 +0530

    fix: image overflow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,10 @@ const Home = ({setPage}) => {
   return (
     <div className='flex justify-center items-center flex-wrap gap-10 mt-10'>
       <div className=' w-[50%] backdrop-blur-md rounded-md p-10 shadow-sm shadow-white'>
-        <img src={pass1Img} alt="pass1 assembler" className={"w-full rounded-md translate-y-10 "} />
+        <div className=' overflow-hidden'>
+        <img src={pass1Img} alt="pass1 assembler" className={"w-full rounded-md translate-y-10  "} />
+        </div>
+        
         <button onClick={()=>setPage("Pass1Editor")} className={' text-xl p-5 w-fit '+ common.Buttonblue}>
               Pass 1: Code to Intermediate File & SYMTAB
           </button>
