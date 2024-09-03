30e6463a2144e619c848a58f8ea8dd27160b536e
Srinivasan <jpsrinivasan38@gmail.com>
2024-09-03 10:47:54 +0530

    design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Home from './components/Home';
 import Pass1Editor from './components/Pass1Editor';
+import { common } from './styles/common';
 
 const App = () => {
   const [page, setPage] = useState("home");
@@ -15,8 +16,8 @@ const App = () => {
     }
   }
   return (
-    <div className='bg-gray-950 w-screen h-screen text-white p-5'>
-      <div onClick={()=>setPage("home")} className='bg-slate-700 p-5 rounded-lg text-2xl font-bold cursor-pointer'>SIC Assembler</div>
+    <div className=" text-white p-5 w-screen h-screen bg-[url('https://firebasestorage.googleapis.com/v0/b/roxssoftware.appspot.com/o/backSpace.jpg?alt=media&token=f8fdd6e5-c240-4a2d-81cc-b72bad0f69b9')] shadow-[inset_0_0_100px_100px_#0c1015] bg-cover bg-[center_right_45em] md:bg-center flex  flex-col">
+      <div onClick={()=>setPage("home")} className={' bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF744_51.02%,#4D78EF11_79.09%)] backdrop-blur-lg shadow-sm shadow-white p-5 rounded-lg text-2xl font-bold cursor-pointer'}><h1 className={common.textNabula}>SIC Assembler</h1></div>
     
     
      {renderPage()}
